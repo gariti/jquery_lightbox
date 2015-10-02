afd165139763e6195bfd4aa5d1b8aa17eff86c5c
Garrett Carver <gariti@gmail.com>
2015-10-02 12:50:26 -0700

    refined lightbox, added caption, and escape event

--- js/app.js
+++ js/app.js
@@ -1,26 +1,38 @@
-//Add a lightbox
-
-//Click image, capture event on link to image
-
-//Show lightbox overlay
-
-//Update overlay with captured link
-
-//Add caption by capturing alt attribute
+//*******Initialize Lightbox Overlay*******
+//create empty lightbox overlay
 var $overlay = $('<div id="overlay"></div>');
-var $image = $("<img>");
 
+//load empty img tag for image to display on lightbox overlay
+var $image = $("<img>");
 $overlay.append($image);
+
+//load empty p tag for caption
+var $caption = $("<p></p>");
+$overlay.append($caption);
+
+//put empty overlay on page (intially hidden)
 $("body").append($overlay);
 
-
+//****When an image on page is clicked, display it on lightbox overlay with caption****
 $("#imageGallery a").click(function(event) {
+  
+  //capture link to clicked image, then put link into img tag attribute so it will display
   event.preventDefault();
   var imagelocation = $(this).attr("href");
-  $image.attr("src", imagelocation)
-  $overlay.fadeIn()
-});
+  $image.attr("src", imagelocation);
 
+  //capturing alt attribute of clicked image and display it as caption
+  var captionText = $(this).children("img").attr("alt");
+  $caption.text(captionText);
+
+  //Show lightbox overlay
+  $overlay.fadeIn();
+
+
+});
+//when clicked, the lightbox overlay will dissapear
 $overlay.click(function() {
   $overlay.fadeOut();
 });
+
+//
